7150572ccdbec12012491835e7d52606eabf92b1
Ainhoaagarciia <ainhoagarciasanz02@gmail.com>
2026-02-19 11:49:02 +0100

    nest gate

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { MovieDto } from './dto/create-movie.dto';
+//import { Movie } from './entities/movies.entity';
+
+@ApiTags('movies')
+@Controller('api/movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  @ApiOkResponse({ type: MovieDto, isArray: true })
+  async getMovies(): Promise<Movie[]> {
+    return this.moviesService.listMovies();
+  }
+  /*async getMovies(): Promise<any> {
+        return [{ name: 'Error intencionado' }];
+    }*/
+  /*async getMovies(): Promise<MovieDto[]> {
+    return this.moviesService.listMovies();
+  }*/
+}
